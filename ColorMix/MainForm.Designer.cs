63b7d3f0faef1d0f4029638d8d96df550de30cd1
Zdeněk Kundrát <zdendaki@zdendaki.net>
2020-02-12 19:13:44 +0100

    Added ColorMix app

---
+++ ColorMix/MainForm.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace ColorMix
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Red = new System.Windows.Forms.TrackBar();
+            this.Green = new System.Windows.Forms.TrackBar();
+            this.Blue = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ColorPanel = new System.Windows.Forms.Panel();
+            this.RedLabel = new System.Windows.Forms.Label();
+            this.GreenLabel = new System.Windows.Forms.Label();
+            this.BlueLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Red)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Green)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Blue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Red
+            // 
+            this.Red.LargeChange = 16;
+            this.Red.Location = new System.Drawing.Point(43, 12);
+            this.Red.Maximum = 255;
+            this.Red.Name = "Red";
+            this.Red.Size = new System.Drawing.Size(190, 45);
+            this.Red.TabIndex = 0;
+            this.Red.TickFrequency = 16;
+            this.Red.Scroll += new System.EventHandler(this.Color_Scroll);
+            // 
+            // Green
+            // 
+            this.Green.LargeChange = 16;
+            this.Green.Location = new System.Drawing.Point(43, 63);
+            this.Green.Maximum = 255;
+            this.Green.Name = "Green";
+            this.Green.Size = new System.Drawing.Size(190, 45);
+            this.Green.TabIndex = 1;
+            this.Green.TickFrequency = 16;
+            this.Green.Scroll += new System.EventHandler(this.Color_Scroll);
+            // 
+            // Blue
+            // 
+            this.Blue.LargeChange = 16;
+            this.Blue.Location = new System.Drawing.Point(43, 114);
+            this.Blue.Maximum = 255;
+            this.Blue.Name = "Blue";
+            this.Blue.Size = new System.Drawing.Size(190, 45);
+            this.Blue.TabIndex = 2;
+            this.Blue.TickFrequency = 16;
+            this.Blue.Scroll += new System.EventHandler(this.Color_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "R";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(11, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "G";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(13, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "B";
+            // 
+            // ColorPanel
+            // 
+            this.ColorPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ColorPanel.Location = new System.Drawing.Point(239, 12);
+            this.ColorPanel.Name = "ColorPanel";
+            this.ColorPanel.Size = new System.Drawing.Size(236, 141);
+            this.ColorPanel.TabIndex = 6;
+            // 
+            // RedLabel
+            // 
+            this.RedLabel.AutoSize = true;
+            this.RedLabel.Location = new System.Drawing.Point(12, 30);
+            this.RedLabel.Name = "RedLabel";
+            this.RedLabel.Size = new System.Drawing.Size(25, 13);
+            this.RedLabel.TabIndex = 7;
+            this.RedLabel.Text = "255";
+            // 
+            // GreenLabel
+            // 
+            this.GreenLabel.AutoSize = true;
+            this.GreenLabel.Location = new System.Drawing.Point(11, 81);
+            this.GreenLabel.Name = "GreenLabel";
+            this.GreenLabel.Size = new System.Drawing.Size(25, 13);
+            this.GreenLabel.TabIndex = 8;
+            this.GreenLabel.Text = "255";
+            // 
+            // BlueLabel
+            // 
+            this.BlueLabel.AutoSize = true;
+            this.BlueLabel.Location = new System.Drawing.Point(12, 132);
+            this.BlueLabel.Name = "BlueLabel";
+            this.BlueLabel.Size = new System.Drawing.Size(25, 13);
+            this.BlueLabel.TabIndex = 9;
+            this.BlueLabel.Text = "255";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 165);
+            this.Controls.Add(this.BlueLabel);
+            this.Controls.Add(this.GreenLabel);
+            this.Controls.Add(this.RedLabel);
+            this.Controls.Add(this.ColorPanel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Blue);
+            this.Controls.Add(this.Green);
+            this.Controls.Add(this.Red);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Míchání barev";
+            ((System.ComponentModel.ISupportInitialize)(this.Red)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Green)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Blue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar Red;
+        private System.Windows.Forms.TrackBar Green;
+        private System.Windows.Forms.TrackBar Blue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel ColorPanel;
+        private System.Windows.Forms.Label RedLabel;
+        private System.Windows.Forms.Label GreenLabel;
+        private System.Windows.Forms.Label BlueLabel;
+    }
+}
+
---
+++ ColorMix/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorMix
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            ChangeColor();
+        }
+
+        private void Color_Scroll(object sender, EventArgs e)
+        {
+            ChangeColor();
+        }
+
+        private void ChangeColor()
+        {
+            ColorPanel.BackColor = Color.FromArgb(Red.Value, Green.Value, Blue.Value);
+
+            RedLabel.Text = Red.Value.ToString();
+            GreenLabel.Text = Green.Value.ToString();
+            BlueLabel.Text = Blue.Value.ToString();
+        }
+    }
+}
