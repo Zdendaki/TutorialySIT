a3f2f6602828f168fdd21b8b5d8ee6f83fd7c24b
Zdeněk Kundrát <zdendaki@zdendaki.net>
2020-02-13 11:38:06 +0100

    Kondiciogram v0.1

---
+++ Kondiciogram/MainForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Kondiciogram
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.VerticalLineAnnotation verticalLineAnnotation1 = new System.Windows.Forms.DataVisualization.Charting.VerticalLineAnnotation();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BirthDate = new System.Windows.Forms.DateTimePicker();
+            this.Graph = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Count = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Graph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Datum narození:";
+            // 
+            // BirthDate
+            // 
+            this.BirthDate.Location = new System.Drawing.Point(104, 12);
+            this.BirthDate.Name = "BirthDate";
+            this.BirthDate.Size = new System.Drawing.Size(200, 20);
+            this.BirthDate.TabIndex = 1;
+            // 
+            // Graph
+            // 
+            this.Graph.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            verticalLineAnnotation1.ClipToChartArea = "ChartArea1";
+            verticalLineAnnotation1.Height = 100D;
+            verticalLineAnnotation1.LineColor = System.Drawing.Color.Red;
+            verticalLineAnnotation1.Name = "Today";
+            this.Graph.Annotations.Add(verticalLineAnnotation1);
+            chartArea1.AxisX.Interval = 3D;
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisY.Interval = 0.5D;
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisY.Maximum = 1D;
+            chartArea1.AxisY.Minimum = -1D;
+            chartArea1.BorderColor = System.Drawing.Color.Gray;
+            chartArea1.Name = "ChartArea1";
+            this.Graph.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.Graph.Legends.Add(legend1);
+            this.Graph.Location = new System.Drawing.Point(15, 121);
+            this.Graph.Name = "Graph";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Fyzický cyklus";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Psychický cyklus";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.Name = "Intelektuální cyklus";
+            this.Graph.Series.Add(series1);
+            this.Graph.Series.Add(series2);
+            this.Graph.Series.Add(series3);
+            this.Graph.Size = new System.Drawing.Size(457, 228);
+            this.Graph.TabIndex = 2;
+            this.Graph.Text = "Graf";
+            // 
+            // Count
+            // 
+            this.Count.Location = new System.Drawing.Point(310, 10);
+            this.Count.Name = "Count";
+            this.Count.Size = new System.Drawing.Size(75, 23);
+            this.Count.TabIndex = 3;
+            this.Count.Text = "Vypočítat";
+            this.Count.UseVisualStyleBackColor = true;
+            this.Count.Click += new System.EventHandler(this.Count_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.Count);
+            this.Controls.Add(this.Graph);
+            this.Controls.Add(this.BirthDate);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Kondiciogram";
+            ((System.ComponentModel.ISupportInitialize)(this.Graph)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker BirthDate;
+        private System.Windows.Forms.DataVisualization.Charting.Chart Graph;
+        private System.Windows.Forms.Button Count;
+    }
+}
+
---
+++ Kondiciogram/MainForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Kondiciogram
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Count_Click(object sender, EventArgs e)
+        {
+            DateTime today = DateTime.Today;
+            int life = (int)(today - BirthDate.Value.Date).TotalDays;
+
+            for (int i = -10; i < 30; i++)
+            {
+                Graph.Series[0].Points.AddXY(DateTime.Today.AddDays(i).ToString("d.M.yyyy"), CountCycle(life + i, 1));
+                Graph.Series[1].Points.AddXY(DateTime.Today.AddDays(i).ToString("d.M.yyyy"), CountCycle(life + i, 2));
+                Graph.Series[2].Points.AddXY(DateTime.Today.AddDays(i).ToString("d.M.yyyy"), CountCycle(life + i, 3));
+
+                //Graph.Annotations[0].Visible = true;
+            }
+        }
+
+        private double CountCycle(int days, int cycleType)
+        {
+            if (cycleType == 1) // Fyzický cyklus
+            {
+                return Math.Sin((days / 23.000) * 2 * Math.PI);
+            }
+            else if (cycleType == 2) // Psychický cylkus
+            {
+                return Math.Sin((days / 28.000) * 2 * Math.PI);
+            }
+            else if (cycleType == 3) // Intelektuální cyklus
+            {
+                return Math.Sin((days / 33.000) * 2 * Math.PI);
+            }
+
+            return 0;
+        }
+    }
+}
